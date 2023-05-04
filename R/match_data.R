b1c0d04ca6866f5a5e8506d7bd41837b92a6557e
athenamelia <tran26h@mtholyoke.edu>
2023-05-04 09:49:42 -0400

    new commit

--- R/match_data.R
+++ R/match_data.R
@@ -7,9 +7,10 @@
 
 match_data <- function(o.data) {
   ps.model <- glm(treat ~ x.1 + x.2 + x.4 + x.5 + x.7 + x.8, family = binomial(link = "logit"), data = o.data)
-  o.data$ps.score <- predict(ps.model, type = "response")
-
-  logitPS <-  log(o.data$ps.score/(1-o.data$ps.score))
+  ps.score <- predict(ps.model, type = "response")
+  o.data$ps.score <- ps.score
+    
+  logitPS <-  log(ps.score/(1-ps.score))
   m.out1 <- MatchIt::matchit(treat ~ x.1 + x.2 + x.4 + x.5 + x.7 + x.8,
                              method = "nearest", distance = "glm", data = o.data,
                              caliper = .2*sd(logitPS), ratio = 1, replace=FALSE)
